829f3054348912c5f2d53ea447d37811888f3778
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-10-10 08:02:25 -0400

    update to challenge

--- Models/PasswordValidator.cs
+++ Models/PasswordValidator.cs
@@ -2,5 +2,8 @@ namespace _028_bb_the_password_validator.Models;
 
 public class PasswordValidator
 {
-    
+    public void CheckPassword(string desiredPassword)
+    {
+        
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 // :: Object Tests ::
 // :: Boss Battle 5/5 ::
 
+using _028_bb_the_password_validator.Models;
+
 Console.Title = "The Password Validator";
 
 Console.Clear();
 
-// Ask for user input
-// Pass through argument to password validator
+// Create instance of validator class
+PasswordValidator validator = new PasswordValidator();
+
+// Gather user input
+Console.Write("Please enter your desired password: ");
+string? desiredPassword = Console.ReadLine();
+
+// Call to password validator through instance
+validator.CheckPassword(desiredPassword);
+
 // Receive response from PasswordValidator class
 // Do it all again
